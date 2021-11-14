0dad20fbbe4a36d031daf8302e026c117d652cca
Farhan Ahmed <farhan.ahmed2@ontariotechu.net>
2021-11-14 10:35:12 -0500

    Update highscorespage.js

---
+++ assets/scripts/highscorespage.js
@@ -0,0 +1,49 @@
+var highScore = document.querySelector("#HighScores");
+
+var clearHighScores = document.querySelector("#clear");
+
+
+clearHighScores.addEventListener("click", function () {
+  localStorage.clear();
+  highscores();
+});
+
+
+highscores();
+
+function highscores() {
+
+  if (localStorage.getItem("High-Scores") === null) {
+    highScore.textContent = "No highscores available.";
+  } else {
+    var HighScoreEL = localStorage.getItem("High-Scores");
+    var HighScoreObject = JSON.parse(HighScoreEL);
+
+    var highscoreLi = document.createElement("ol");
+    highscoreLi.classList.add("highscoreBackground");
+    highScore.appendChild(highscoreLi);
+    var questionList;
+
+
+    if (HighScoreObject.length) {
+
+      for (var x in HighScoreObject) {
+        questionList = document.createElement("li");
+        questionList.innerHTML = `initials: ${HighScoreObject[x].initials} | score: ${HighScoreObject[x].score}`;
+        highscoreLi.appendChild(questionList);
+
+      }
+
+    } else {
+
+      questionList = document.createElement("li");
+      questionList.innerHTML = `initials: ${HighScoreObject.initials} | score: ${HighScoreObject.score}`;
+      highscoreLi.appendChild(questionList);
+
+    }
+
+  }
+
+}
+
+		
